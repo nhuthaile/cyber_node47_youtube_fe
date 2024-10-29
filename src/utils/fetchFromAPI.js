6411fa5d9d529240547722e1ccc01ff23610ccc6
HAI LE <nhuthai.le@gmail.com>
2024-10-29 09:05:45 +0700

    update

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,19 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = "http://localhost:8080";
 
 const options = {
   params: {
     maxResults: 50,
   },
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
     // 'token': localStorage.getItem("LOGIN_USER")
   },
 };
-
-
 
 export const fetchFromAPI = async (url) => {
   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
@@ -21,3 +19,53 @@ export const fetchFromAPI = async (url) => {
   return data;
 };
 
+// Define function call API get list video from BE
+export const getVideoAPI = async () => {
+  try {
+    const videoList = await axios.get(`${BASE_URL}/video/get-videos`);
+    // console.log("videoList", videoList);
+    return videoList;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+};
+
+// Define function call API get list type videos
+
+export const getListTypeVideo = async () => {
+  try {
+    const listTypeVideo = await axios.get(`${BASE_URL}/video/get-types`);
+    return listTypeVideo;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// Define function call API get video by type
+
+export const getVideoByType = async (typeId) => {
+  try {
+    const videoList = await axios.get(`${BASE_URL}/video/get-videos/${typeId}`);
+    console.log("videoList", videoList);
+    return videoList;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+};
+
+// Define function call API video by ID
+
+export const getVideoById = async (videoId) => {
+  try {
+    const videoById = await axios.get(
+      `${BASE_URL}/video/video-detail/${videoId}`
+    );
+    console.log("videoById", videoById);
+    return videoById;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+};
